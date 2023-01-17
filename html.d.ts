a22d54b7b9a1cf4891ee30acb044143c44d853c8
Stan Miagchilo <stasss95@mail.ru>
2023-01-17 21:25:34 +0100

    first commit

---
+++ html.d.ts
@@ -0,0 +1,14 @@
+
+declare module '@vue/runtime-dom' {
+    interface HTMLAttributes {
+        [key: string]: any
+    }
+}
+
+declare module '@vue/runtime-core' {
+    interface AllowedComponentProps {
+        [key: string]: any
+    }
+}
+
+export { }
